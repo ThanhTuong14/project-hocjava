fd7f270c578f375da07c407787a7197de42ca454
ThanhTuong14 <thanhtuong1805@gmail.com>
2024-10-14 16:14:03 +0700

    init project

---
+++ src/hocjava/HelloWorld.java
@@ -0,0 +1,10 @@
+package hocjava;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		System.out.print("Hello World");
+		System.out.print("Hello World");
+	}
+
+}
